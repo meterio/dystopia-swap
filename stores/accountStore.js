a33a4a973d6258bb39eccc75c50df7b35d43b9dc
yanfeizuo <hh18856856693@gmail.com>
2023-08-04 06:50:17 +0800

    fix social media;update subgraph

--- stores/accountStore.js
+++ stores/accountStore.js
@@ -224,20 +224,20 @@ class Store {
   };
 
   getGasPrice = async (speed) => {
-    let gasSpeed = speed;
-    if (!speed) {
-      gasSpeed = this.getStore("gasSpeed");
-    }
+    // let gasSpeed = speed;
+    // if (!speed) {
+    //   gasSpeed = this.getStore("gasSpeed");
+    // }
 
-    try {
-      const web3 = await this.getHttpWeb3Provider();
-      const gasPrice = await web3.eth.getGasPrice();
-      const gasPriceInGwei = web3.utils.fromWei(gasPrice, "gwei");
-      return gasPriceInGwei;
-    } catch (e) {
-      console.log(e);
-      return {};
-    }
+    // try {
+    //   const web3 = await this.getHttpWeb3Provider();
+    //   const gasPrice = await web3.eth.getGasPrice();
+    //   const gasPriceInGwei = web3.utils.fromWei(gasPrice, "gwei");
+    //   return gasPriceInGwei;
+    // } catch (e) {
+    //   console.log(e);
+    //   return {};
+    // }
   };
 
   getHttpWeb3Provider = () => {
