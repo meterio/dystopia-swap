75b39a0b3d654f23daac7757b7901d0055b48a36
yanfeizuo <hh18856856693@gmail.com>
2023-10-10 07:52:20 +0800

    update airdrop root

--- stores/configurations/airdrop.js
+++ stores/configurations/airdrop.js
@@ -1,6 +1,6 @@
 
 export const root = "0x5faf03f11c4285ae17e4219ea12e0358e0bedbc70e1e47ca78325bed512a31b9"
-export const root2 = "0x60324d5923ca8804d4eece3bb64c7fc0a7ffed885c3dd46843b34760f3433354"
+export const root2 = "0x29a55e76145ad8e386904c990db90e275c938d3d63690e1675591b20292f6920"
 
 export default [
 
