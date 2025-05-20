f2eaedae7431d08a86cc2ea0d924f7819c8ae921
yanfeizuo <18856856693@163.com>
2025-05-20 16:54:58 +0800

    fix build err:found page without a React Component as default export in pages/walletConfig

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,25 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import classes from "./home/home.module.css";
 
 import { createAppKit } from '@reown/appkit/react';
-import { networks, projectId, metadata, ethersAdapter } from './walletConfig';
+
+import { meter, meterTestnet, theta } from '@reown/appkit/networks'
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+
+// Get projectId from https://cloud.reown.com
+const projectId = "31995a32c9e9c2876df4f27830f9c941"
+
+// Create a metadata object - optional
+const metadata = {
+  name: 'Voltswap',
+  description: 'Voltswap',
+  url: 'https://voltswap.finance',
+  icons: ['https://raw.githubusercontent.com/meterio/token-list/refs/heads/master/data/VOLT/logo.png']
+}
+
+// for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
+const networks = [meter, theta, meterTestnet]
+
+const ethersAdapter = new EthersAdapter();
 
 createAppKit({
   adapters: [ethersAdapter],
